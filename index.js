77dfa2e77e8076870edbb54ddda22bce6ff2e7d3
Kiril Kobzar <kirilkobzar@gmail.com>
2022-01-07 11:39:55 +0100

    get product list

--- index.js
+++ index.js
@@ -26,7 +26,33 @@ try{
     throw new TypeError(e)
 }
 
-//--- City menu
+//---get all products list
+let productList = []
+for (let cty in menu.products){
+    if (!Object.entries(menu.products[cty]).length){
+        continue;
+    }
+    for(let prod in menu.products[cty]){
+        if (!productList.includes(prod)){
+            productList.push(prod)
+        }
+    }
+}
+
+//---Get product info
+function getProductInfo(city = '', product = '') {
+    if (!city || !product)
+        return false
+
+
+}
+
+/*
+*
+* ---CITY MENU
+*
+* */
+
 let citiesMenuArr = []
 
 if (menu.cities){
@@ -45,7 +71,15 @@ function showCityMenu(ctx) {
     ctx.reply(`Hello, ${ctx.chat.id}! \nChoose your city:`,cityMenu)
 }
 
-//--- Product menu
+/*
+* ---END CITY MENU
+* */
+
+/*
+*
+* ---PRODUCT MENU
+*
+* */
 
 function getMenu(city = '') {
     if (!city)
@@ -55,18 +89,19 @@ function getMenu(city = '') {
 
     if (menu.products){
         for (let cty in menu.products){
+            //if city index is same as param get all products
             if (cty === city){
                 for (let prod in menu.products[cty]){
-                    //console.log(menu.products[cty][prod].title)
-                    //console.log(cty)
-                    //console.log(prod)
-                    productMenuArr.push([Markup.button.callback(menu.products[cty][prod].title, prod)])
+                    // check if product name is provided
+                    if (menu.products[cty][prod].title === undefined){
+                        productMenuArr.push([Markup.button.callback('🔞🥶🥵', prod)])
+                    }else {
+                        productMenuArr.push([Markup.button.callback(menu.products[cty][prod].title, prod)])
+                    }
                 }
-                //console.log(menu.products)
             }
         }
     }
-    //console.log(productMenuArr)
 
     if (productMenuArr.length){
         return productMenuArr
@@ -75,6 +110,9 @@ function getMenu(city = '') {
     }
 }
 
+/*
+* ---END PRODUCT MENU
+* */
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
@@ -86,9 +124,12 @@ store(false)
 bot.start((ctx) => {
     showCityMenu(ctx)
 })
-bot.help((ctx) => ctx.reply('Help message'))
+
+bot.help((ctx) => ctx.reply('Contacts: @d33kei'))
 bot.on('message', (ctx) => ctx.telegram.sendMessage(ctx.message.chat.id, ctx.message))
 bot.action('delete', (ctx) => ctx.deleteMessage())
+
+//---HOME BUTTON
 bot.action('main', (ctx) => {
     ctx.deleteMessage()
     store(ctx.chat.id,false)
@@ -111,6 +152,19 @@ for (let cty in menu.cities){
 }
 
 //--- Products order bind
+
+for (let prod of productList){
+    console.log(prod)
+    bot.action(prod,ctx=>{
+        if (store.has(ctx.chat.id) && store.get(ctx.chat.id).city){
+            ctx.deleteMessage()
+            ctx.reply(`User id: ${ctx.chat.id}\nCity: ${store.get(ctx.chat.id).city} \nPay now`)
+        }else {
+            ctx.sendMessage(ctx.chat.id,'Ой...  Выберите город')
+            showCityMenu(ctx)
+        }
+    })
+}
 
 bot.action('xtc-1',ctx=>{
     if (store.has(ctx.chat.id) && store.get(ctx.chat.id).city){
